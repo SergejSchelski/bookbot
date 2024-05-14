4a1d0ba47763bc431180a479e8dfd38763c2185f
SergejSchelski <sergej.schelski@campus.tu-berlin.de>
2024-05-14 13:04:50 +0200

    Added function that counts characters and prints a dictionary of the characters and their frequency in the text.

--- main.py
+++ main.py
@@ -3,15 +3,40 @@ def main():
     # Opens up the file and reads it in to the contents variable
         file_contents = f.read()
         #print(file_contents) #Prints contents variable to the console
+
         #prints the number of words
-        number_of_words = count(file_contents)
-        print(f"The book conatains {number_of_words} words.")
+        #number_of_words = count(file_contents)
+        #print(f"The book conatains {number_of_words} words.")
+
+        #print a dictionary with characters in the text and how often they appeared
+        print(f"The following characters were used in the text with the following numbers: {let_dict(file_contents)}")
         
+
+
 def count(text):
      #splits the file contents into words and puts them all in a list
      word_list = text.split()
     #count function returns the number of items in the word_list
      return len(word_list)
+
+
+
+def let_dict(text):
+     #converts all upper case to lower case letters
+     low_text = text.lower()
+     #initialize a dictionary
+     dic = {}
+     #loops over all characters in the text string, adds the character as a key with value 1 if it is new
+     #increases the value by 1 if it is not new
+     for letter in low_text:
+          #if the letter is already a key in the dictionary, increase it's value by 1
+          if letter in dic:
+               dic[letter] += 1
+          #else add the key to dic and make it's value 1
+          else:
+               dic[letter] = 1
+     return dic
+
 
 # This protective if condition ensures 'main()' is called at the right moment.
 if __name__ == '__main__':
